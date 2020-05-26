2c028507cd061db852daf081d7dfa860c7e2b81a
Alexander <sunchos89@mail.ru>
2020-05-26 11:20:34 +0300

    Modifiy example PoissonExtractSurface from https://lorensen.github.io/VTKExamples/site/Cxx/Points/PoissonExtractSurface/

--- example/Example.cxx
+++ example/Example.cxx
@@ -36,38 +36,222 @@
 #include <vtkXMLPolyDataWriter.h>
 
 #include "vtkPoissonReconstruction.h"
+#include <filesystem>
+#include <iostream>
+
+#include <vtkSmartPointer.h>
+
+#include <vtkBYUReader.h>
+#include <vtkOBJReader.h>
+#include <vtkPLYReader.h>
+#include <vtkPolyDataReader.h>
+#include <vtkSTLReader.h>
+#include <vtkXMLPolyDataReader.h>
+
+#include <vtkPointSource.h>
+#include <vtkPoissonReconstruction.h>
+#include <vtkPCANormalEstimation.h>
+
+#include <vtkPointData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkCamera.h>
+
+#include <vtkNamedColors.h>
+#include <vtksys/SystemTools.hxx>
+
+//int main(int argc, char *argv[])
+//{
+//  //if ( argc < 4 )
+//  //  {
+//  //  cout << "PoissonReconstruction takes 3 arguments: " << endl;
+//  //  cout << "1-Input file (*.vtp)" << endl;
+//  //  cout << "2-Depth" << endl;
+//  //  cout << "3-Output file (*.vtp)" << endl;
+//  //  return EXIT_FAILURE;
+//  //  }
+//  std::string inputFileName = "D:/programming/C++/VTK/New folder/PoissonReconstruction-master/build/bin/Debug/horsePoints.vtp";  //"horsePoints.vtp";
+//  std::string outputFileName = "D:/programming/C++/VTK/New folder/PoissonReconstruction-master/build/bin/Debug/horse.vtp"; //"horse.vtp";
+//  int            depth = 10;
+//
+//  vtkSmartPointer< vtkXMLPolyDataReader > reader =
+//    vtkSmartPointer< vtkXMLPolyDataReader >::New();
+//  reader->SetFileName(inputFileName.c_str());
+//  reader->Update();
+//  if (!reader && !reader->GetOutputPort())
+//      return -1;
+//  vtkSmartPointer< vtkPoissonReconstruction > poissonFilter =
+//    vtkSmartPointer< vtkPoissonReconstruction >::New();
+//  poissonFilter->SetDepth(depth);
+//  poissonFilter->SetInputConnection( reader->GetOutputPort() );
+//  poissonFilter->Update();
+//
+//  vtkSmartPointer< vtkXMLPolyDataWriter > writer =
+//    vtkSmartPointer< vtkXMLPolyDataWriter >::New();
+//  writer->SetInputConnection( poissonFilter->GetOutputPort() );
+//  writer->SetFileName(outputFileName.c_str());
+//  writer->Update();
+//
+//  return EXIT_SUCCESS;
+//}
+
+namespace
+{
+    vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName);
+}
 
 int main(int argc, char *argv[])
 {
-  if ( argc < 4 )
+    vtkSmartPointer<vtkPolyData> polyData = ReadPolyData("D:/programming/C++/VTK/New folder/PoissonReconstruction-master/build/bin/Debug/sphere_points.vtp");
+    std::cout << "# of points: " << polyData->GetNumberOfPoints() << std::endl;
+
+    vtkSmartPointer<vtkPoissonReconstruction> surface =
+        vtkSmartPointer<vtkPoissonReconstruction>::New();
+    surface->SetDepth(12);
+
+    int sampleSize = polyData->GetNumberOfPoints() * .00005;
+    if (sampleSize < 10)
     {
-    cout << "PoissonReconstruction takes 3 arguments: " << endl;
-    cout << "1-Input file (*.vtp)" << endl;
-    cout << "2-Depth" << endl;
-    cout << "3-Output file (*.vtp)" << endl;
-    return EXIT_FAILURE;
+        sampleSize = 10;
+    }
+    if (polyData->GetPointData()->GetNormals())
+    {
+        std::cout << "Using normals from input file" << std::endl;
+        surface->SetInputData(polyData);
+    }
+    else
+    {
+        std::cout << "Estimating normals using PCANormalEstimation" << std::endl;
+        vtkSmartPointer<vtkPCANormalEstimation> normals =
+            vtkSmartPointer<vtkPCANormalEstimation>::New();
+        normals->SetInputData(polyData);
+        normals->SetSampleSize(sampleSize);
+        normals->SetNormalOrientationToGraphTraversal();
+        normals->FlipNormalsOff();
+        surface->SetInputConnection(normals->GetOutputPort());
     }
 
-  std::string inputFileName = argv[1];  //"horsePoints.vtp";
-  std::string outputFileName = argv[3]; //"horse.vtp";
-  int            depth = atoi(argv[2]);
+    vtkSmartPointer<vtkPolyDataMapper> surfaceMapper =
+        vtkSmartPointer<vtkPolyDataMapper>::New();
+    surfaceMapper->SetInputConnection(surface->GetOutputPort());
 
-  vtkSmartPointer< vtkXMLPolyDataReader > reader =
-    vtkSmartPointer< vtkXMLPolyDataReader >::New();
-  reader->SetFileName( inputFileName.c_str() );
-  reader->Update();
+    vtkSmartPointer<vtkNamedColors> colors =
+        vtkSmartPointer<vtkNamedColors>::New();
 
-  vtkSmartPointer< vtkPoissonReconstruction > poissonFilter =
-    vtkSmartPointer< vtkPoissonReconstruction >::New();
-  poissonFilter->SetDepth(depth);
-  poissonFilter->SetInputConnection( reader->GetOutputPort() );
-  poissonFilter->Update();
+    vtkSmartPointer<vtkProperty> back =
+        vtkSmartPointer<vtkProperty>::New();
+    back->SetColor(colors->GetColor3d("banana").GetData());
 
-  vtkSmartPointer< vtkXMLPolyDataWriter > writer =
-    vtkSmartPointer< vtkXMLPolyDataWriter >::New();
-  writer->SetInputConnection( poissonFilter->GetOutputPort() );
-  writer->SetFileName( outputFileName.c_str() );
-  writer->Update();
+    vtkSmartPointer<vtkActor> surfaceActor =
+        vtkSmartPointer<vtkActor>::New();
+    surfaceActor->SetMapper(surfaceMapper);
+    surfaceActor->GetProperty()->SetColor(colors->GetColor3d("Tomato").GetData());
+    surfaceActor->SetBackfaceProperty(back);
 
-  return EXIT_SUCCESS;
+    // Create graphics stuff
+    //
+    vtkSmartPointer<vtkRenderer> ren1 =
+        vtkSmartPointer<vtkRenderer>::New();
+    ren1->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
+    vtkSmartPointer<vtkRenderWindow> renWin =
+        vtkSmartPointer<vtkRenderWindow>::New();
+    renWin->AddRenderer(ren1);
+    renWin->SetSize(512, 512);
+
+    vtkSmartPointer<vtkRenderWindowInteractor> iren =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    iren->SetRenderWindow(renWin);
+
+    // Add the actors to the renderer, set the background and size
+    //
+    ren1->AddActor(surfaceActor);
+
+    // Generate an interesting view
+    //
+    ren1->ResetCamera();
+    ren1->GetActiveCamera()->Azimuth(120);
+    ren1->GetActiveCamera()->Elevation(30);
+    ren1->GetActiveCamera()->Dolly(1.0);
+    ren1->ResetCameraClippingRange();
+
+    renWin->Render();
+    iren->Initialize();
+    iren->Start();
+
+    return EXIT_SUCCESS;
+}
+
+namespace
+{
+    vtkSmartPointer<vtkPolyData> ReadPolyData(const char *fileName)
+    {
+        vtkSmartPointer<vtkPolyData> polyData;
+        std::string extension = vtksys::SystemTools::GetFilenameExtension(std::string(fileName));
+        if (extension == ".ply")
+        {
+            vtkSmartPointer<vtkPLYReader> reader =
+                vtkSmartPointer<vtkPLYReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+            polyData = reader->GetOutput();
+        }
+        else if (extension == ".vtp")
+        {
+            vtkSmartPointer<vtkXMLPolyDataReader> reader =
+                vtkSmartPointer<vtkXMLPolyDataReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+            polyData = reader->GetOutput();
+        }
+        else if (extension == ".vtk")
+        {
+            vtkSmartPointer<vtkPolyDataReader> reader =
+                vtkSmartPointer<vtkPolyDataReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+            polyData = reader->GetOutput();
+        }
+        else if (extension == ".obj")
+        {
+            vtkSmartPointer<vtkOBJReader> reader =
+                vtkSmartPointer<vtkOBJReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+            polyData = reader->GetOutput();
+        }
+        else if (extension == ".stl")
+        {
+            vtkSmartPointer<vtkSTLReader> reader =
+                vtkSmartPointer<vtkSTLReader>::New();
+            reader->SetFileName(fileName);
+            reader->Update();
+            polyData = reader->GetOutput();
+        }
+        else if (extension == ".g")
+        {
+            vtkSmartPointer<vtkBYUReader> reader =
+                vtkSmartPointer<vtkBYUReader>::New();
+            reader->SetGeometryFileName(fileName);
+            reader->Update();
+            polyData = reader->GetOutput();
+        }
+        else
+        {
+            vtkSmartPointer<vtkPointSource> points =
+                vtkSmartPointer<vtkPointSource>::New();
+            points->SetNumberOfPoints(1000);
+            points->SetRadius(1.0);
+            points->SetCenter(vtkMath::Random(-1, 1),
+                vtkMath::Random(-1, 1),
+                vtkMath::Random(-1, 1));
+            points->SetDistributionToShell();
+            points->Update();
+            polyData = points->GetOutput();
+        }
+        return polyData;
+    }
 }
